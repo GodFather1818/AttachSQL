54fa0f0ae901ce08847cdb14603d95252d1bd5a5
Codester01 <dhruvgangar1234@gmail.com>
2024-08-02 18:40:59 +0530

    Your commit message

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './category.schema';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(@InjectModel(Category.name) private categoryModel: Model<Category>) {}
+
+  
+  async getCategories(): Promise<Category[]> {
+    return this.categoryModel.find().exec();
+  }
+}
