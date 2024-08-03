deac3625681028aca354e346c3ae8f5693073c6c
parita2003 <paritamukeshmend@gmail.com>
2024-08-03 22:46:32 +0530

    Styling added in products hero section

--- task1/app/products/page.tsx
+++ task1/app/products/page.tsx
@@ -30,18 +30,19 @@ const CategoryPage = () => {
   }, []);
 
   return (
-    <div className="container">
-      <h1>Products</h1>
-      <Link href="/products/add"><Button className='btn-add' onClick={add}>Add</Button></Link>
-      <div className="grid">
+    <div className="w-full container m-5 border rounded-lg align-middle p-10 mx-auto  bg-gradient-to-r from-blue-100 to-blue-200 shadow-xl hover:shadow-2xl transition duration-300 ease-in-out">
+      <div className='flex justify-between items-center mt-3 mb-10'>
+        <h1 className='text-5xl text-center text-primary font-bold '>Products</h1>
+        <Link href="/products/add"><Button className='btn-add p-3 text-lg bg-primary text-white hover:bg-blue-100 hover:text-blue-950 border rounded-lg' onClick={add}>+ Add Product</Button></Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6">
         {products.map(product => (
-          <div key={product.id} className="card">
-            <img src={`http://localhost:3001/${product.bannerImage}`} alt={product.name} style={{
-              margin:'auto',
-              width:'30%'}}/>
-            <h2>{product.name}</h2>
-            <h3>Category:{product.category}</h3>
-            <p>Description:{product.description}</p>
+          <div key={product.id} className="card bg-slate-100 bg-white shadow-md rounded-lg p-4 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+            <img className="image-container h-48 w-full flex items-center justify-center bg-gray-200"
+ src={`http://localhost:3001/${product.bannerImage}`} alt={product.name} />
+            <h2 className='font-bold text-blue-900 text-2xl mt-5'>{product.name}</h2>
+            <h3 className='font-semibold text-blue-500 text-lg m-1'>Category:{product.category}</h3>
+            <p className='text-blue-900'>Description:{product.description}</p>
           </div>
         ))}
       </div>
