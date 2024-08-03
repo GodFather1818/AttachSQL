adbf2cce47103965fd095b1df65b982031973d43
parita2003 <paritamukeshmend@gmail.com>
2024-08-03 23:06:53 +0530

    updated delete in both products and category

--- task1/app/products/page.tsx
+++ task1/app/products/page.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import './style.category.css'
 import { Button } from '@mui/material';
 import Link from 'next/link';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const CategoryPage = () => {
   const [products, setProducts] = useState<Category[]>([]);
@@ -37,12 +38,15 @@ const CategoryPage = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6">
         {products.map(product => (
-          <div key={product.id} className="card bg-slate-100 bg-white shadow-md rounded-lg p-4 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+          <div key={product.id} className="card relative bg-slate-100 bg-white shadow-md rounded-lg p-4 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+            <Button onClick={() => { }} className="absolute bottom-0 right-0 mb-2 mr-2 bg-primary">
+              <DeleteIcon sx={{ color: 'red' }} />
+            </Button>
             <img className="image-container h-48 w-full flex items-center justify-center bg-gray-200"
  src={`http://localhost:3001/${product.bannerImage}`} alt={product.name} />
             <h2 className='font-bold text-blue-900 text-2xl mt-5'>{product.name}</h2>
-            <h3 className='font-semibold text-blue-500 text-lg m-1'>Category:{product.category}</h3>
-            <p className='text-blue-900'>Description:{product.description}</p>
+            <h3 className='font-semibold text-blue-500 text-lg m-1'>Category: {product.category}</h3>
+            <p className='text-blue-900'>Description: {product.description}</p>
           </div>
         ))}
       </div>
