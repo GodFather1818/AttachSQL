9fd05d7cc25e298414367ac4a1c74cb53a9af933
Codester01 <dhruvgangar1234@gmail.com>
2024-08-06 15:57:00 +0530

    Swagger API Documentation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CategoryService } from './category/category.service';
-import { CategoryController } from './category/category.controller';
 import { CategoryModule } from './category/category.module';
-// import { ProjectModule } from './project/project.module';
-import { TaskService } from './task/task.service';
-import { TaskController } from './task/task.controller';
 import { TaskModule } from './task/task.module';
-// import { PrjectModule } from './prject/prject.module';
-import { ProjectService } from './project/project.service';
-import { ProjectController } from './project/project.controller';
 import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [UserModule,CategoryModule,ProductModule, MongooseModule.forRoot('mongodb+srv://Dhruv:Dhruv@cluster0.h6fh7fu.mongodb.net/task1?retryWrites=true&w=majority'), TaskModule, ProjectModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    CategoryModule,
+    ProductModule,
+    TaskModule,
+    ProjectModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
