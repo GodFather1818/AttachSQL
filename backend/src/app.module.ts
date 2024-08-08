43cf2da0b3fbcd96a6796743a098a4522b88d44b
Codester01 <dhruvgangar1234@gmail.com>
2024-08-08 16:06:26 +0530

    Packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,33 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryService } from './category/category.service';
+import { CategoryController } from './category/category.controller';
 import { CategoryModule } from './category/category.module';
+// import { ProjectModule } from './project/project.module';
+import { TaskService } from './task/task.service';
+import { TaskController } from './task/task.controller';
 import { TaskModule } from './task/task.module';
+// import { PrjectModule } from './prject/prject.module';
+import { ProjectService } from './project/project.service';
+import { ProjectController } from './project/project.controller';
 import { ProjectModule } from './project/project.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_KEY } from './constant';
+
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env.local',
-      isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URL'),
-      }),
-      inject: [ConfigService],
-    }),
-    UserModule,
-    CategoryModule,
-    ProductModule,
-    TaskModule,
-    ProjectModule,
-  ],
+  imports: [CategoryModule,ProductModule, MongooseModule.forRoot('mongodb+srv://Dhruv:Dhruv@cluster0.h6fh7fu.mongodb.net/task1?retryWrites=true&w=majority'), TaskModule, ProjectModule, AuthModule, JwtModule.register({
+    global: true,
+    secret: JWT_KEY,
+    signOptions: {
+      expiresIn: '30d'
+    }
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
